ab40a2bda5df4f72cb72253fed4a7349292b3c48
kitiphichet-del <kitiphichet@gmail.com>
2026-09-25 14:42:41 +0700

    Fix ML Kit DownloadConditions import

--- app/src/main/java/com/trilingo/interpreter/MainActivity.java
+++ app/src/main/java/com/trilingo/interpreter/MainActivity.java
@@ -24,7 +24,7 @@ import android.widget.Toast;
 
 import com.google.mlkit.nl.languageid.LanguageIdentification;
 import com.google.mlkit.nl.languageid.LanguageIdentifier;
-import com.google.mlkit.nl.translate.DownloadConditions;
+import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
 import com.google.mlkit.nl.translate.Translation;
 import com.google.mlkit.nl.translate.Translator;
