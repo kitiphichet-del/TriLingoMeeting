074c7f5dab4572a337ad33f80afc982aa9af884a
kitiphichet-del <kitiphichet@gmail.com>
2026-09-25 17:47:47 +0700

    Bump TriLingo Meeting to v1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.trilingo.interpreter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.3.0"
+        versionCode = 14
+        versionName = "1.4.0"
     }
 
     compileOptions {
